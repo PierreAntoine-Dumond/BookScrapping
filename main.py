bb91d6314f2408652bc41f016113959a473fcd35
PierreAntoine Dumond <contact@destineedev.com>
2024-03-09 22:55:46 +0100

    Ajout de la librairie BeautifulSoup et Première ligne de code

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import requests 
+import requests
+from bs4 import BeautifulSoup
+import csv
 
 url = "https://books.toscrape.com/"
 response = requests.get(url)
 
 if response.status_code == 200:
     html = response.text
-    print(html)
+    # print(html)
+    f = open("booktoscrape", "w")
+    f.write(html)
+    f.close()
+
+    soup = BeautifulSoup(html, 'html5lib')
+    soup.find()
 else:
     print('ERREUR', response.status_code)
 
